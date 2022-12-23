d4d748cad6d59e9136fdc1b8e0c7b268f90b3f2b
tatadavila <tatdev91@gmail.com>
2022-12-23 10:50:36 -0500

    implement browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+// @vendors
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+// @components
 import App from './App'
+
+// @styles
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
